407fe4a9c358bb1b246e99795b0aa8c56dbd6e0f
Pavel <geksbomba@gmail.com>
2023-10-03 20:28:15 +0600

    added comments

--- backend/api/api_phone.py
+++ backend/api/api_phone.py
@@ -5,6 +5,19 @@ from fastapi import APIRouter
 phone_router = APIRouter()
 
 
+#  endpoint for getting phone and address
 @phone_router.get("/check_data")
-async def root():
+async def check_data():
+    return {"message": "Hello World"}
+
+
+#  endpoint for creating phone and address
+@phone_router.post("/write_data")
+async def write_data():
+    return {"message": "Hello World"}
+
+
+#  endpoint for updating phone and address
+@phone_router.put("/write_data")
+async def update_data():
     return {"message": "Hello World"}
