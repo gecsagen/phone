e69018f1546db1aed5479d6e4f552c0daf9f8253
Charlie <geksbomba@gmail.com>
2023-10-04 10:04:21 +0600

    improved database operating methods and endpoints

--- backend/api/dals.py
+++ backend/api/dals.py
@@ -3,14 +3,6 @@
 import redis
 from schemas import FullDate, Phone, Address
 
-r = redis.Redis()
-r.mset({"Croatia": "Zagreb", "Bahamas": "Nassau"})
-
-print(r.get("Bahamas"))
-
-from typing import Union
-from uuid import UUID
-
 
 class PersonalInfo:
     """Class for working with data at the database level"""
@@ -20,9 +12,13 @@ class PersonalInfo:
 
     # Creating a record with personal information
     async def create_personal_info(self, data: FullDate) -> None:
-        self.db_session.mset({"phone":[data.number, data.address]})
+        self.db_session.mset({data.number: data.address})
 
     # Receiving a record with personal information
-    async def get_personal_info(phone: Phone) -> Address:
-        self.db_session
-        
+    async def get_personal_info(self, phone: Phone) -> Address:
+        address = self.db_session.get(phone)
+        return Address(full_address=address)
+
+    # update a record with personal information
+    async def update_personal_info(self, data: FullDate) -> None:
+        self.db_session.hmset(data.number, data.model_dump(exclude_unset=True))
--- backend/api/services.py
+++ backend/api/services.py
@@ -1,17 +1,22 @@
 """The module contains service functions"""
 from .schemas import Phone, Address, FullDate
+from dals import PersonalInfo
 
 
 #  service for obtaining an address
 async def _check_data(phone: Phone) -> Address:
-    return {"message": phone}
+    personal_dal = PersonalInfo()
+    personal_date = await personal_dal.get_personal_info(phone=phone)
+    return personal_date
 
 
 #  service for adding phone number and address
-async def _write_data(phone_address: FullDate):
-    return {"message": FullDate(number="test", address="test")}
+async def _write_data(full_date: FullDate):
+    personal_dal = PersonalInfo()
+    await personal_dal.create_personal_info(data=full_date)
 
 
 #  service for updating phone number and address
-async def _update_data(phone_address: FullDate):
-    return {"message": FullDate(number="test", address="test")}
+async def _update_data(full_date: FullDate):
+    personal_dal = PersonalInfo()
+    await personal_dal.update_personal_info(data=full_date)
